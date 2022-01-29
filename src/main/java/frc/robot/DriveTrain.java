17afacc77cb0c055f4237997522ce8a8f576dc2e
Liam <liam.wirth1@outlook.com>
2022-01-29 14:21:25 -0800

    drive train with tank drive is done.

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -1,15 +1,22 @@
 package frc.robot;
-import java.net.CacheRequest;
+
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup; //SpeedController has been depricated, a new change for 2022. I'll use the new method.
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.GenericHID;
+
 public class DriveTrain {
      final private CANSparkMax lbMotor, lmMotor, lfMotor; //Left Back, Left Middle, Left Front.
      final private CANSparkMax rbMotor, rmMotor, rfMotor; //Right Back, Right Middle, Right Front
      final private MotorControllerGroup leftMotors, rightMotors;
+     final private DifferentialDrive dDrive = new DifferentialDrive(leftMotors, rightMotors);
+
+     //TODO make a constants class, i'm not willing to do that now tho.
+
+
     public DriveTrain(){
         //left motors + motorcontrol group.
         lbMotor = new CANSparkMax(3,MotorType.kBrushless);
@@ -28,11 +35,17 @@ public class DriveTrain {
                 index.setSecondaryCurrentLimit(45);
                 index.setIdleMode(IdleMode.kBrake);
             }
-                    
+        
     }
 
-    public void run(){  
-        //to be written
+    public void run(){ 
+        double rAxis = Robot.driverController.getY(Hand.kRight);
+        double lAxis = Robot.driverController.getY(Hand.kLeft);
+        dDrive.tankDrive(lSpeed, rSpeed);
+        
+    }
+    public void swerveDrive(){
+        //TODO Liam will write swerveDrive when he has the time
     }
 
     public void closeMotors(){ //vscode tells me there is a resource leak, so this should fix it?
